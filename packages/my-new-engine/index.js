12e7ea88ccdf6f19e844009b8d9e44223fae41e9
Allen Zhou <azhou@squareup.com>
2018-06-28 14:34:18 -0700

    Update package.json

--- packages/my-new-engine/index.js
+++ packages/my-new-engine/index.js
@@ -2,5 +2,9 @@ const EngineAddon = require('ember-engines/lib/engine-addon');
 
 module.exports = EngineAddon.extend({
   name: 'my-new-engine',
-  lazyLoading: true
+  lazyLoading: true,
+  // eslint-disable-next-line ember/avoid-leaking-state-in-ember-objects
+  babel: {
+    plugins: ['transform-object-rest-spread']
+  }
 });
